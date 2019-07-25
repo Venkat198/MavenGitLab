e0e6b7f5faa501b551400907950398183e9cc9e3
Kapalli <venkata-subba-raju.kapalli@dxc.com>
2019-07-25 20:37:48 +0530

    First Maven Folder Structure

---
+++ FirstMavenGit/src/main/java/FirstMavenGIt/App.java
@@ -0,0 +1,13 @@
+package FirstMavenGIt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
